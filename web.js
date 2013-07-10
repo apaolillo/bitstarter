321b4eb80dc83a6e43effb805556dd6f4bf423b8
Antonio Paolillo <apaolill@gmail.com>
2013-07-10 10:32:25 +0000

    Updated web.js to read from a file.

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var fs = require('fs');
+
+var buf = new Buffer(fs.readFileSync('index.html'));
+var mybuf = buf.toString();
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(mybuf);
 });
 
 var port = process.env.PORT || 5000;
